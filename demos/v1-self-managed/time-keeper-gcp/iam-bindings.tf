34e3fc4e580341be176bab3486da431d3d102967
Tim Hiatt <timhiatt@google.com>
2022-11-29 14:47:10 +0000

    Adding IAM Bindings

--- demos/v1-self-managed/time-keeper-gcp/iam-bindings.tf
+++ demos/v1-self-managed/time-keeper-gcp/iam-bindings.tf
@@ -22,6 +22,20 @@ resource "google_project_iam_member" "gke_service_account" {
   member  = "serviceAccount:${google_service_account.gke_service_account.email}"
 }
 
+resource "google_project_iam_member" "gke_service_account_worker" {
+  count   = length(local.GKEServiceAccountIAMRoles)
+  project = google_project.project.project_id
+  role    = element(local.GKEServiceAccountIAMRoles, count.index)
+  member  = "serviceAccount:${google_service_account.gke_worker_service_account.email}"
+}
+
+resource "google_project_iam_member" "gke_service_account_external" {
+  count   = length(local.GKEServiceAccountIAMRoles)
+  project = google_project.project.project_id
+  role    = element(local.GKEServiceAccountIAMRoles, count.index)
+  member  = "serviceAccount:${google_service_account.gke_egress_service_account.email}"
+}
+
 resource "google_project_iam_member" "gitlab_service_account" {
   count   = length(local.GitLabServiceAccountIAMRoles)
   project = google_project.project.project_id
