fc5b1876a810b6f143e0d0e7a4ce959edfbcf136
Tim Hiatt <timhiatt@google.com>
2022-11-30 15:21:19 +0000

    Adding fix

--- bigbash.sh
+++ bigbash.sh
@@ -1,5 +1,11 @@
 
+for cluster in $(gcloud container clusters list --format='csv[no-heading](name,zone, endpoint)  --project="$1"' )
+do
+    clusterName=$(echo $cluster | cut -d "," -f 1)
+    clusterZone=$(echo $cluster | cut -d "," -f 2)
+    clusterEndpoint=$(echo $cluster | cut -d "," -f 3)
 
+    gcloud container clusters get-credentials $clusterName --region="$clusterZone" --project="$1"
+    
 
-gcloud container clusters list --project="$1" --format="csv(name,location)"
-#gcloud container clusters get-credentials ${GKE_CLUSTER_NAME} --region="${GKE_CLUSTER_REGION}" --project="$1"
+done
