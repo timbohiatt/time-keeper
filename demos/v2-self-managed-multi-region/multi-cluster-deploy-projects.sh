fe16f5d48950fc77d3b57ffeac6a3ea38d2b00c3
Tim Hiatt <timhiatt@google.com>
2022-11-30 18:00:23 +0000

    Adding Execute Status

--- demos/v2-self-managed-multi-region/multi-cluster-deploy-projects.sh
+++ demos/v2-self-managed-multi-region/multi-cluster-deploy-projects.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 PROJECT=$1
 
-for cluster in $(gcloud container clusters list --format='csv[no-heading](name,zone, endpoint)  --project="${PROJECT}"' )
+for cluster in $(gcloud container clusters list --format='csv[no-heading](name,zone, endpoint)' --project="${PROJECT}" )
 do
     echo $cluster
 
