b5b51de4e0f49120b0313cb6b0698d76cbf81358
Tim Hiatt <timhiatt@google.com>
2022-12-01 12:45:28 +0000

    Adding Biling Export API

--- demos/v2-self-managed-multi-region/time-keeper-gcp/service-accounts.tf
+++ demos/v2-self-managed-multi-region/time-keeper-gcp/service-accounts.tf
@@ -1,3 +1,9 @@
+resource "google_service_account" "billing_data_admin" {
+  project      = google_project.project.project_id
+  account_id   = "${var.prefix}-${var.env}-billing-data"
+  display_name = "${var.prefix}-${var.demo_name}-${var.env}-billing-admin"
+}
+
 resource "google_service_account" "gke_service_account" {
   project      = google_project.project.project_id
   account_id   = "${var.prefix}-${var.env}-gke"
